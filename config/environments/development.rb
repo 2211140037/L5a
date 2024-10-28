15495048e09c123f225df7cc1fb77dd3a7e9838b
Tokiya Fukuda <2211140037x@ed.fuk.kindai.ac.jp>
2024-10-28 10:34:55 +0000

    push to render.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.hosts << "0e8d763fabad4666b44058f65f40d7a8.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "8ad512e339e44954853a9c0f43739dc7.vfs.cloud9.us-east-1.amazonaws.com"
 end
